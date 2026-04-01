e1e5128e1ffc09e98aec09e9a07f5646294037ad
raj171804-eng <raj171804@gmail.com>
2026-04-01 21:27:45 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("KC mockup loaded");
+function login() {
+  const role = document.getElementById("role").value;
+
+  if (role === "Admin") {
+    window.location.href = "admin.html";
+  } else {
+    window.location.href = "agent.html";
+  }
+}
